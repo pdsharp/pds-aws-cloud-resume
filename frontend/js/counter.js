92514d8bc4258bdb6fa5ca7ed8bfff981961d8b5
Patrick Sharp <pdsharp@gmail.com>
2022-02-18 17:40:56 -0800

    js fixes

--- frontend/js/counter.js
+++ frontend/js/counter.js
@@ -1,19 +1,6 @@
-const functionApi = 'https://7jsyoco0gb.execute-api.us-west-2.amazonaws.com/Prod/count'
-
-const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApi)
-    .then(response => {
-        return response.text()
-    })
-    .then(response => {
-        console.log("Website called function API.",response);
-        count = response;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
-        console.log(error);
-      });
-    return count;
+const getCount = () => {
+  fetch("https://7jsyoco0gb.execute-api.us-west-2.amazonaws.com/Prod/count")
+  .then(response => response.text())
+  .then(response => document.getElementById('counter').innerText = response)
 }
-
-getVisitCount();
+getCount();
